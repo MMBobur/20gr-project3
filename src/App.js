acb546256fa4dbbb142f1a3556c8993802528f43
azizbekcodemy <azizbekobidiov0303@gmail.com>
2023-01-19 12:55:33 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
+import Futter from "./components/futer";
+import PastNavbar from "./components/pastNavbar";
+import Ortga from "./components/Orqaga";
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import Router from "./Router";
 import "./style.css"
@@ -11,10 +14,13 @@ function App() {
     <>
       <div>
         <BrowserRouter>
-        <div style={{display:"flex"}}>
-           <Navbar />
-           <Router/>
-        </div>
+          <div style={{ display: "flex" }}>
+            <Navbar />
+            <Router />
+          </div>
+          <Futter />
+          <PastNavbar />
+          <Ortga />
         </BrowserRouter>
       </div>
     </>
